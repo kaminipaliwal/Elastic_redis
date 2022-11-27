e849ce5570d916d197f3291cf5584d3b5ca1b368
KaminiPaliwal05 <kaminip.codetown@gmail.com>
2022-11-27 17:34:21 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+
+// searching on query
+app.get('/search/:index/:type', async (req, res) => {
+  const { phraseSearch } = require('./index');
+  const data = await phraseSearch(req.params.index, req.params.type, req.query.q);
+  res.json(data);
+});
+
+app.listen(3333, () => console.log('server running at 3333'));
+
